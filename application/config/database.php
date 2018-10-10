7480c05e5fb062b5e9df0eb9edd1eb0e59f2af30
frdev <felipertw2@gmail.com>
2018-10-09 21:56:13 -0300

    # senha BD

--- application/config/database.php
+++ application/config/database.php
@@ -70,14 +70,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group  = 'default';
 $query_builder = TRUE;
+$senha         = $_SERVER['SERVER_NAME'] == 'localhost' ? '' : 'harmonia';
 
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => '',
+	'password' => $senha,
 	'database' => 'rh',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
