e0e0a9947b2045413fdcc30e16361fc81b291181
frdev <felipertw2@gmail.com>
2018-10-09 22:07:25 -0300

    #Ajuste HOST

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/rhharmonia';
+$host               = $_SERVER['NAME'] == 'localhost' ? $_SERVER['HTTP_HOST'].'/rhharmonia' : $_SERVER['HTTP_HOST'];
+$config['base_url'] = 'http://'.$host;
 
 /*
 |--------------------------------------------------------------------------
